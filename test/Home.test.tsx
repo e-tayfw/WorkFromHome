6ec8f9a5d9e55abd315dacf0a6cafa17c1ef29fb
e-tayfw <ethantay321@gmail.com>
2024-09-17 11:42:27 +0800

    edited test case

--- test/Home.test.tsx
+++ test/Home.test.tsx
@@ -1,17 +1,18 @@
 // import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Home from "@/pages/index"
+import Home from '@/pages/index'
 import { describe, expect } from "@jest/globals";
 
 
 
 describe("Home", () => {
   test("renders the Home component correctly", () => {
-    const { getByRole } = render(<Home />);
-
-    // Example: Checking if an element with role 'main' exists
-    const mainElement = getByRole("main"); // Adjust the selector as needed
-    expect(mainElement).toBeTruthy();
+    render(<Home />);
+  
+      // Use getAllByText to find all elements containing the text "Schedule"
+      const scheduleElements = screen.getAllByText(/schedule/i); // Returns an array of elements
+        // Check that at least one "Schedule" element is found
+      expect(scheduleElements.length).toBeGreaterThan(0);
   });
 });
