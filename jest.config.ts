75f162dea9a9227667f1b2d8689f2d7d679b7ef6
e-tayfw <121296660+e-tayfw@users.noreply.github.com>
2024-09-17 11:59:47 +0800

    Configured jest into project (#5)
    * added jest sample test with configuration to project
    
    * added jest sample test with configuration to project
    
    Reconfigured the jest configuration and pipeline included jest
    
    * Reconfigured and installed new packages for jsdom testing
    
    Reconfigured and installed new packages for jsdom testing
    
    * Update package.json removed duplicates
    
    * changed babel configuration, add components
    
    * edited test case
    
    ---------
    
    Co-authored-by: ETHAN TAY FU WEN <e_tayfw@MacBook-Pro.local>
    Co-authored-by: neojw1505 <70019047+neojw1505@users.noreply.github.com>

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config = {
+  preset: "ts-jest", // Use ts-jest to work with TypeScript
+  testEnvironment: "jest-environment-jsdom", // Use jsdom for React component testing
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"], // Optional: Path to setup file
+};
+// module.exports = {
+//   preset: "ts-jest", // Use ts-jest to work with TypeScript
+//   testEnvironment: "jsdom", // Use jsdom for React component testing
+//   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"], // Optional: Path to setup file
+//   transform: {
+//     "^.+\\.[tj]sx?$": "babel-jest", // Use babel-jest to transform JS/TS files, including JSX
+//   },
+//   moduleNameMapper: {
+//     "^@/(.*)$": "<rootDir>/src/$1", // Adjust path alias to your src folder
+//   },
+//   // transformIgnorePatterns: ["<rootDir>/node_modules/"], // Ignore transforming node_modules
+// };
+
+module.exports = createJestConfig(config)
