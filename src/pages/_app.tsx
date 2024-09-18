3ccff0ff1c9b6c41200e7b7915e5ddb1b30fbbce
songdre123 <129580329+songdre123@users.noreply.github.com>
2024-09-19 01:40:09 +0800

    Setup navs and login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,8 @@ import dynamic from "next/dynamic";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
 import { ReactNode, useState, useEffect } from "react";
-import Nav from "@/components/nav";
+import Nav from "@/components/Nav";
+
 export default function App({ Component, pageProps }: AppProps) {
   const [userType, setUserType] = useState<string | null>(null);
 
