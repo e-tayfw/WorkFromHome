c91804a54ec3823aa9254d1b4eed3c613b9ec1fd
e-tayfw <ethantay321@gmail.com>
2024-09-18 18:12:22 +0800

    Add test schedule data controller and route

--- backend-api/routes/api.php
+++ backend-api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\TestScheduleController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -12,3 +13,4 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::get('/employees', [EmployeeController::class, 'getAllEmployees']);
 Route::get('/employee/id/{id}', [EmployeeController::class, 'getEmployeeById']);
 Route::get('/employee/email/{email}', [EmployeeController::class, 'getEmployeeByEmail']);
+Route::get(uri: '/testScheduleData', action: [TestScheduleController::class, 'getSchedule']);
