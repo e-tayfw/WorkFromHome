c373bef0dfdf96c775f1f9f14b02bc6600d4848f
e-tayfw <121296660+e-tayfw@users.noreply.github.com>
2024-09-19 13:25:28 +0800

    config: tailwind add colours (#6)
    * added jest sample test with configuration to project
    
    * added jest sample test with configuration to project
    
    Reconfigured the jest configuration and pipeline included jest
    
    * Reconfigured and installed new packages for jsdom testing
    
    Reconfigured and installed new packages for jsdom testing
    
    * Update package.json removed duplicates
    
    * changed babel configuration, add components
    
    * edited test case
    
    * Added colors to tailwind config
    
    ---------
    
    Co-authored-by: ETHAN TAY FU WEN <e_tayfw@MacBook-Pro.local>
    Co-authored-by: neojw1505 <70019047+neojw1505@users.noreply.github.com>
    Co-authored-by: songdre123 <129580329+songdre123@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,11 +9,22 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // Light mode colors
+        text: '#090a0c',
+        background: '#fbfcfd',
+        primary: '#072040',
+        secondary: '#a2b4cc',
+        accent: '#819dc1',
+        // Dark mode colors
+        'dark-text': '#d0dbfc',
+        'dark-background': '#12141a',
+        'dark-primary': '#006a66',
+        'dark-secondary': '#ffffff',
+        'dark-accent': '#436be5',
+        }
+        
       },
     },
-  },
   plugins: [],
 };
 export default config;
