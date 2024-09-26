8b6cf460df99c5e67e9fa28b32bb821b93fff23c
elliot <elliot.tan.2022@scis.smu.edu.sg>
2024-09-27 00:33:33 +0800

    feat(api): complete backend endpoint for creating request
    - Added API functionality to create a new request with staff and approver details
    - Created request and request log records
    - Started implementing modal to display success or failure on request submission

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {
@@ -24,10 +25,10 @@ const config: Config = {
         }
       },
     },
-  },
   plugins: [
-    require('flowbite/plugin'),
+    require('flowbite/plugin')
   ],
   darkMode: 'class', // Enables dark mode with a 'dark' class on the root element
 };
+
 export default config;
