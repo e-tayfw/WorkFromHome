abe1ad147ead23cae65062f17e6ab586aaa002d5
Zavier <127392910+zavierr01@users.noreply.github.com>
2024-09-28 23:13:02 +0800

    feat: create History page and add global session for employee ID (#15)
    * feat: added manage requests page
    
    - Added /history route to access requests
    - RequestEntry & RequestTable components created for history
    - Currently using static data from constants
    
    * feat: Integrated History page with backend
    
    - Added session global variable for employeeid
    - Minor UI update on history page
    - Updated nav bar links to apply and history page

--- backend-api/app/Http/Controllers/RequestController.php
+++ backend-api/app/Http/Controllers/RequestController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Employee;
 use App\Models\Requests;
 
 
@@ -36,36 +35,36 @@ class RequestController extends Controller
         }
     }   
 
-    public function createRequest($request_info)
-    {   
-        // Decode json input and give assign to variables based on key
-        $decoded_request = json_decode($request_info);
+    // public function createRequest($request_info)
+    // {   
+    //     // Decode json input and give assign to variables based on key
+    //     $decoded_request = json_decode($request_info);
 
-        $staff_id = $decoded_request->staffid;
-        $selectedDate = $decoded_request->date;
-        $arrangement = $decoded_request->preferredArrangement;
-        $reason = $decoded_request->reason;
+    //     $staff_id = $decoded_request->staffid;
+    //     $selectedDate = $decoded_request->date;
+    //     $arrangement = $decoded_request->preferredArrangement;
+    //     $reason = $decoded_request->reason;
 
-        // Fetch employee row using staff_id
-        $employee = Employee::where("Staff_ID", $staff_id)->first();
+    //     // Fetch employee row using staff_id
+    //     $employee = Employee::where("Staff_ID", $staff_id)->first();
 
-        // If found retrieve the staff's Reporting Manager
-        if ($employee) {
+    //     // If found retrieve the staff's Reporting Manager
+    //     if ($employee) {
             
-            $reporting_manager = $employee->Reporting_Manager;
+    //         $reporting_manager = $employee->Reporting_Manager;
 
-            // If Reporting Manager found, update the DB with the request
-            if ($reporting_manager) {
-                // Create new Request row
-                $newRequest = new Requests();
-                $newRequest -> Requestor_ID = $staff_id;
-                $newRequest -> Approver_ID = $reporting_manager;
-                $newRequest -> Status = "Pending";
-                $newRequest -> Date_Requested = $selectedDate;
-                $newRequest -> Date_Of_Request = date("Y-m-d");
-                $newRequest -> Duration = $arrangement;
+    //         // If Reporting Manager found, update the DB with the request
+    //         if ($reporting_manager) {
+    //             // Create new Request row
+    //             $newRequest = new Requests();
+    //             $newRequest -> Requestor_ID = $staff_id;
+    //             $newRequest -> Approver_ID = $reporting_manager;
+    //             $newRequest -> Status = "Pending";
+    //             $newRequest -> Date_Requested = $selectedDate;
+    //             $newRequest -> Date_Of_Request = date("Y-m-d");
+    //             $newRequest -> Duration = $arrangement;
 
-                $newRequest-> save();
+    //             $newRequest-> save();
 
                 // Create new Request Log row
                 // $newRequestLog = new 
