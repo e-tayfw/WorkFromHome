a9c3b43da899546ec3d186002052d4f9d6daf3d1
Elliot Tan <83326039+Somnus22@users.noreply.github.com>
2024-10-18 00:37:43 +0800

    feat(DateRangePicker): Implement custom DateRangePickerComponent

--- src/components/apply/index.tsx
+++ src/components/apply/index.tsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect, useCallback } from "react";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import { Datecomponent } from "@/components/apply/datepicker";
 import { Selection } from "@/components/apply/selection";
 import { Reason } from "@/components/apply/reason";
 import { Submit } from "@/components/apply/submit";
+import { DateRangePickerComponent } from "@/components/apply/range_datepicker";
 import { H1, BodyLarge, Body, Display } from "@/components/TextStyles";
 import Swal from 'sweetalert2';
 import { useSelector } from "react-redux";
@@ -10,6 +13,11 @@ import axios from 'axios';
 import { toast } from "react-toastify";
 
 type ArrangementType = 'AM' | 'PM' | 'FD' | '';
+
+interface DateRange {
+  start: string;
+  end: string;
+}
 
 interface SubmitData {
   staffid: number;
@@ -24,6 +32,7 @@ const Apply: React.FC = () => {
   const [reason, setReason] = useState("");
   const [isFormValid, setIsFormValid] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [dateRange, setDateRange] = useState<DateRange>({start: '', end: ''});
   const staffid = useSelector((state: any) => state.auth.staffId);
 
   useEffect(() => {
@@ -166,6 +175,12 @@ const Apply: React.FC = () => {
     }
   }, [staffid, selectedDate, preferredArrangement, reason]);
 
+  const handleDateRangeChange = (newDateRange: DateRange) => {
+    setDateRange(newDateRange);
+    console.log('Date range changed:', newDateRange);
+    // You can perform additional actions here, like fetching data based on the new date range
+  };
+  
   return (
     <div className="container flex flex-col items-center px-4 sm:px-6 lg:px-8 mt-24 sm:mt-28">
       <div className="">
@@ -174,45 +189,65 @@ const Apply: React.FC = () => {
         </Display>
       </div>
       <div className="w-full max-w-lg">
-        <div>
-          <Body className="text-sm font-light text-primary">
-            Select a date 
-          </Body>
-        </div>
-        <div className="mt-2">
-          <Datecomponent
-            onDateChange={handleDateChange}
-            selectedDate={selectedDate}
-          />
-        </div>
+        <Tabs>
+          <TabList>
+            <Tab>Ad-Hoc</Tab>
+            <Tab>Recurring</Tab>
+          </TabList>
 
-        <div className="mt-8">
-          <Body className="text-sm font-light text-primary">
-            Preferred Work Arrangement 
-          </Body>
-        </div>
-        <div className="mt-2">
-          <Selection
-            onSelectionChange={handleArrangementChange}
-            selectedValue={preferredArrangement}
-          />
-        </div>
+          <TabPanel>
+            <div>
+              <Body className="text-sm font-light text-primary mt-4">
+                Select a date 
+              </Body>
+              <div className="mt-2">
+                <Datecomponent
+                  onDateChange={handleDateChange}
+                  selectedDate={selectedDate}
+                />
+              </div>
 
-        <div className="mt-8">
-          <Body className="text-sm font-light text-primary">Reason</Body>
-        </div>
-        <div className="mt-2">
-          <Reason onReasonChange={handleReasonChange} reasonText={reason} />
-        </div>
+              <Body className="text-sm font-light text-primary mt-8">
+                Work Arrangement 
+              </Body>
+              <div className="mt-2">
+                <Selection
+                  onSelectionChange={handleArrangementChange}
+                  selectedValue={preferredArrangement}
+                />
+              </div>
 
-        <div className="flex items-center text-left bg-gray-50 border border-gray-300 text-primary text-sm font-light mt-4 h-8 px-2 py-3 rounded" role="alert">
-          <svg className="fill-current w-4 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
-          <p>All fields must be filled</p>
-        </div>
+              <Body className="text-sm font-light text-primary mt-8">Reason</Body>
+              <div className="mt-2">
+                <Reason onReasonChange={handleReasonChange} reasonText={reason} />
+              </div>
 
-        <div className="mt-4">
-          <Submit onSubmit={handleSubmit} isDisabled={!isFormValid || isLoading} />
-        </div>
+              <div className="flex items-center text-left bg-gray-50 border border-gray-300 text-primary text-sm font-light mt-4 h-8 px-2 py-3 rounded" role="alert">
+                <svg className="fill-current w-4 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
+                <p>All fields must be filled</p>
+              </div>
+              <div className="mt-4">
+                <Submit onSubmit={handleSubmit} isDisabled={!isFormValid || isLoading} />
+              </div>
+            </div>
+          </TabPanel>
+
+          <TabPanel>
+            <div>
+              <Body className="text-sm font-light text-primary mt-4">
+                  Select a Start Date and End Date
+              </Body>
+
+              <div className="mt-2">
+                <DateRangePickerComponent 
+                  selectedDateRange={dateRange} 
+                  onDateRangeChange={handleDateRangeChange}
+                />
+              </div>
+            </div>
+
+          </TabPanel>
+        </Tabs>
       </div>
     </div>
   );
