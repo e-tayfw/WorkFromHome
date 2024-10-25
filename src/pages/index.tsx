65f0cdf43d1b91944f296b8ddee9388eab6ab1fd
Elliot Tan <83326039+Somnus22@users.noreply.github.com>
2024-10-25 14:28:14 +0800

    Merge dev_wfh into Ticket-28 (#32)
    * feat(ci): add GitHub Actions workflow for Laravel CI/CD with deployment to EC2
    
    * test: validate on push trigger backend CI/CD
    
    * chore(env): add .env.testing and update CI workflow for testing environment
    
    * chore(ci): dynamically generate .env and .env.testing
    
    * test: validate on push trigger backend CI/CD
    
    * chore(ci): update Laravel CI/CD workflow
    
    * test: validate on push trigger backend CI/CD
    
    * As an employee, I want to see my name so that I know I’m in the correct account (#29)
    
    * feat + fix: Add name visible in Nav and Fix Auth routing issues
    
    * feat: Front end fixes, shows users on that day clearer
    
    * test: added automated frontend test case for Nav
    
    * Fix: Fix routing issue and Fix styling of table in modal
    
    * Fix: Styling issue with id number
    
    ---------
    
    Co-authored-by: e-tayfw <ethantay321@gmail.com>
    
    * chore(ci): update ci/cd workflow for backend (#31)
    
    * chore(ci): update Laravel CI/CD workflow
    
    * test: validate on push trigger backend CI/CD
    
    * Update backend.yaml
    
    * Create test.txt
    
    * Update backend.yaml
    
    * Update test.txt
    
    * chore(ci): update ci/cd workflow for backend
    
    ---------
    
    Co-authored-by: neojw1505 <70019047+neojw1505@users.noreply.github.com>
    Co-authored-by: songdre123 <129580329+songdre123@users.noreply.github.com>
    Co-authored-by: e-tayfw <ethantay321@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,28 @@
 import { useState, useEffect } from "react";
-import { Auth } from "@/components/auth";
+import { useRouter } from "next/router";
 
 const Home = () => {
   const [userType, setUserType] = useState<string | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
     setUserType(localStorage.getItem("userType"));
   }, []);
 
-  return (
-    <div>
-      {!userType && <Auth />} 
-    </div> 
-  );
-}
+  useEffect(() => {
+    // Check if the current path is "/" and userType is available
+    if (router.pathname === "/" && userType) {
+      router.push("/schedule");
+    }
+
+    // If userType is null, route them to "/auth"
+    if (!userType) {
+      router.push("/auth");
+    }
+  }, [router, userType]);
+
+  // If already on a valid route or if no redirection is needed, return null
+  return null;
+};
 
 export default Home;
