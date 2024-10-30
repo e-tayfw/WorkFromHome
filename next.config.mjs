61ea1b8a136d80380194c3815a8f09367c28972f
e-tayfw <ethantay321@gmail.com>
2024-10-31 00:04:22 +0800

    vercel: reconfig next.js for CORS

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['workfromhomebucket.s3.ap-southeast-2.amazonaws.com'],
+    domains: ["workfromhomebucket.s3.ap-southeast-2.amazonaws.com"],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
